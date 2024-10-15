0f54d1ec2a40ec255a2faac71ec9710437ecd564
Arif <arifurrahmancmt2017@gmail.com>
2024-10-16 03:27:09 +0600

    second commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () => "Hello this is the HomePages");
+
+var products = new List<Product>()
+{
+    new Product("Samsung s15", 15250),
+    new Product("Redmi 8A",12450),
+};
+
+app.MapGet("/products",()=>
+{
+    return Results.Ok(products);
+});
 
 app.Run();
+public record Product(string Name, decimal Price);
