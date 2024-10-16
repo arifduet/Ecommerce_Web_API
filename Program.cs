4ea7cb5e7924fd2010a95061c09b9cb3d65aeba8
Arif <arifurrahmancmt2017@gmail.com>
2024-10-17 02:23:08 +0600

    init commit3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if(app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -12,18 +12,60 @@ if(app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+List<Category> categories = new List<Category>();
+
 app.MapGet("/", () => "Hello this is the HomePages");
 
-var products = new List<Product>()
+//GET: /api/categories => Read categories.
+app.MapGet("/api/categories", () =>
 {
-    new Product("Samsung s15", 15250),
-    new Product("Redmi 8A",12450),
-};
-
-app.MapGet("/products",()=>
-{
-    return Results.Ok(products);
+    return Results.Ok(categories);
 });
 
+//POST: /api/categories => Create categories.
+app.MapPost("/api/categories", () =>
+{
+    var newCategory = new Category
+    {
+        CategoryId = Guid.Parse("12345678"),
+        Name = "Electronics",
+        Description = "Devices and Gadgets including phone",
+        CreateAt = DateTime.UtcNow,
+    };
+    categories.Add(newCategory);
+    return Results.Created($"/api/categories/{newCategory.CategoryId}", newCategory);
+});
+
+//DELETE: /api/categories => Delete categories.
+app.MapDelete("/api/categories", () =>
+{
+    var foundCategory=categories.FirstOrDefault(Category => Category.CategoryId == Guid.Parse("12345678"));
+    if (foundCategory == null)
+    {
+        return Results.NotFound("Category doesn't found for delete");
+    }
+    categories.Remove(foundCategory);
+    return Results.NoContent();
+});
+
+//PUT: /api/categories => Update a categories.
+app.MapPut("/api/categories", () =>
+{
+    var foundCategory=categories.FirstOrDefault(Category => Category.CategoryId == Guid.Parse("12345678"));
+    if (foundCategory == null)
+    {
+        return Results.NotFound("Category doesn't found");
+    }
+    foundCategory.Name ="Smart Phone";
+    foundCategory.Description = "This is the update smart phone";
+    return Results.NoContent();
+});
 app.Run();
-public record Product(string Name, decimal Price);
+public record Category
+{
+    public Guid CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreateAt { get; set; }
+
+};
